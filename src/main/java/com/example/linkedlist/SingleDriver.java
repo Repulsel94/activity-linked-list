7555dfe691a934ebead0496ad8045274d65a9c7c
Repulsel94 <glory4205@gmail.com>
2023-03-21 19:32:18 -0700

    Initial commit

---
+++ src/main/java/com/example/linkedlist/SingleDriver.java
@@ -0,0 +1,26 @@
+package com.example.linkedlist;
+
+public class SingleDriver {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> list = new SinglyLinkedList<>();
+
+        list.addFirst("first");
+        list.addFirst("second");
+        list.addFirst("third");
+        list.addFirst("fourth");
+        list.addFirst("fifth");
+
+        list.addLast("sixth");
+        list.addLast("seventh");
+        list.addLast("eighth");
+
+        System.out.println(list);
+
+        System.out.println(list.peekFirst());
+        System.out.println(list.peekLast());
+
+
+    }
+}
