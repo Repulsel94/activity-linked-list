3ec46cd7fc7ffdaf041141d82a5936b235eb57f4
Repulsel94 <glory4205@gmail.com>
2023-03-23 19:36:48 -0700

    linkedlist

--- src/main/java/com/example/linkedlist/SingleDriver.java
+++ src/main/java/com/example/linkedlist/SingleDriver.java
@@ -17,9 +17,29 @@ public class SingleDriver {
         list.addLast("eighth");
 
         System.out.println(list);
+        System.out.println(list.size());
 
         System.out.println(list.peekFirst());
         System.out.println(list.peekLast());
+        System.out.println(list.size());
+
+        System.out.println(list.pollFirst());
+        System.out.println(list);
+        System.out.println(list.size());
+
+        System.out.println(list.pollLast());
+        System.out.println(list);
+        System.out.println(list.size());
+
+
+        System.out.println(list.contains("fifth"));
+        System.out.println(list.contains("first"));
+        System.out.println(list.contains("seventh"));
+        System.out.println(list.contains("fourth"));
+
+        list.clear();
+        System.out.println(list);
+        System.out.println(list.size());
 
 
     }
